2b32ea05ea8401c5182c7d43bf4e055d8a01baa0
Chintan <munjanichintan@gmail.com>
2021-02-05 16:47:34 +0530

    Basic functionality added

---
+++ authen/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class SignUpForm(UserCreationForm):
+	full_name = forms.CharField(max_length=100, help_text='Required. 100 characters of fewer.')
+
+	class Meta:
+		model = CustomUser
+		fields = UserCreationForm.Meta.fields + ('full_name', 'age',)
